db651d59241d7c1d80b7e01fa30492ee153aeb14
+ <+>
2022-02-22 11:52:36 -0500

    commit tree 5ce65b83383ebffa2df3f46250e2756e321ccbf8

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
+  if (someNumber < 400)
+  return "This one is on me!"
+    else if (someNumber > 2500)
+    return  'No can do.'; 
+    else  (someNumber > 2000)
+    return 'I will gladly take your thirty bucks.';
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === 'NYC'? "Ok, sounds good." : "No go."
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous': 
+      return "Thank you."
+    default:
+      return "Bye."
+  }
   // Write your code here!
 }
